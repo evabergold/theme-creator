489eb0267a449ec8877abc662e33a0a851e06651
Eva Bergold <evabergold@gmail.com>
2024-04-30 14:38:54 +0200

    finish task 2: add color

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
+import ColorForm from "./Components/ColorForm/ColorForm.jsx";
+import { uid } from "uid";
+import { useState } from "react";
 
 function App() {
+  const [colors, setColors] = useState(initialColors);
+
+  function handleAddColor(newColor) {
+    setColors([{ id: uid(), ...newColor }, ...colors]);
+  }
+
   return (
     <>
       <h1>Theme Creator</h1>
 
-      {initialColors.map((color) => {
+      <ColorForm onAddColor={handleAddColor} />
+      <br />
+
+      {colors.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
     </>
